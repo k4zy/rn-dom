4ebbebe702f7ac9db50be70bb01dcda406a70c41
kazuki-yoshida <kzk.yshd@gmail.com>
2018-08-11 17:25:53 +0900

    Set up typescript env

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,6 +1,12 @@
 "use strict";
 
 var config = {
+  getTransformModulePath() {
+    return require.resolve("react-native-typescript-transformer");
+  },
+  getSourceExts() {
+    return ["ts", "tsx", "js", "jsx"];
+  },
   getPlatforms() {
     return ["dom"];
   },
